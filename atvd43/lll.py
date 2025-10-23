e5ce2e6f86cea976bd57cc608d0a895253d56d96
Lucas <davilucas007124@gmail.com>
2025-10-23 13:33:28 -0300

    Update lll.py

--- atvd43/lll.py
+++ atvd43/lll.py
@@ -1,6 +1,10 @@
 # Contador de Cliques em um Botão (Simulado)
 # Simule um contador que pergunte quantas vezes o
 #  botão foi clicado e mostre mensagens como “Clique 1 registrado”, “Clique 2 registrado” até o número informado.
+cliques = int(input("Quantos cliques? "))
+
+for i in range(1, cliques + 1):
+    print(f"Clique {i} registrado")
 
 
 
